73ffee210470cbb81c0ba6c7d87ddb797d9bba10
Vladimir Makaev <vmakaev@gmail.com>
2013-02-21 03:23:22 +0300

    fix 2 build warnings

--- Source/.common/CommonAssemblyInfo.cs
+++ Source/.common/CommonAssemblyInfo.cs
@@ -1,9 +1,4 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
-
-using System;
-using System.Reflection;
 using System.Runtime.InteropServices;
 
 [assembly: AssemblyProduct("TestDataCreator")]
